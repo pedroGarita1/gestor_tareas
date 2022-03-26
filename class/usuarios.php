cfcffb8d7e5ebf0efd2361c46f48b9e1d41a7265
pedroGarita1 <pedrogarit3@gmail.com>
2022-03-25 19:42:38 -0600

    correccion_nombre_query_insertar

--- class/usuarios.php
+++ class/usuarios.php
@@ -21,7 +21,7 @@
         public function registrar_usuario($datos){
             $C = new Conector();
             $conexion = $C->conexion();
-            $sql = "INSERT INTO t_usuarios (usuario,password,nombre,apellido_parerno,apellido_materno)
+            $sql = "INSERT INTO t_usuarios (usuario,password,nombre,apellido_paterno,apellido_materno)
                     VALUES ('$datos[0]','$datos[1]','$datos[2]','$datos[3]','$datos[4]')";
             $respuesta = mysqli_query($conexion,$sql);
             return $respuesta;
